a4b465e878beb5de258e86d6e1baf874839a6a26
Kim Carter <Kim.Carter@binarymist.net>
2018-12-12 23:52:27 +1300

    Moved files around

--- selenium-standalone-provisioner/index.js
+++ selenium-standalone-provisioner/index.js
@@ -3,23 +3,29 @@ const internals = {};
 
 internals.seleniumHubServiceName = 'selenium-hub';
 
-internals.developmentDeploySeleniumStandalones = async (seleniumStandalones) => {
-  const numberOfRequestedStandalones = seleniumStandalones.length;
+internals.developmentDeploySeleniumStandalones = async (dTO) => {
+  const numberOfRequestedStandalones = dTO.length;
   if (numberOfRequestedStandalones < 1 || numberOfRequestedStandalones > 12) throw new Error(`The number of selenium nodes requested was: ${numberOfRequestedStandalones}. The supported number of testSessions is from 1-12`);
 
   const http = axios.create({baseURL: 'http://docker-compose-ui:5000/api/v1', headers: {'Content-type': 'application/json'}});
 
   const browserCounts = {};
-  for (let i = 0; i < seleniumStandalones.length; i += 1) {
-    browserCounts[seleniumStandalones[i]] = 1 + (browserCounts[seleniumStandalones[i]] || 0);
+  for (let i = 0; i < dTO.length; i += 1) {
+    browserCounts[dTO[i]] = 1 + (browserCounts[dTO[i]] || 0);
   }
 
   const promisedResponses = Object.keys(browserCounts).map(b => http.put('/services', {service: b, project: 'selenium-standalone', num: browserCounts[b]}));
   let successful;
-  await Promise.all(promisedResponses).then((responses) => {
-    successful = responses.every(e => e.status === 200);
-  });
-  return successful;
+  await Promise.all(promisedResponses);
+
+  const containers = await http.get('/projects/selenium-standalone');
+  const seleniumStandaloneServiceNames = containers.data.containers.map(c => c.name);
+
+  const itemsCopy = [...items];
+
+  Array.from(dTO, b => )
+
+
 };
 
 internals.productionDeploySeleniumStandalones = async (event) => {
@@ -30,6 +36,7 @@ internals.productionDeploySeleniumStandalones = async (event) => {
 exports.provisionSeleniumStandalones = async (event, context) => {
   const env = process.env.NODE_ENV;
   const { seleniumHubServiceName } = internals;
+  const {  } = event;
   //const response = {};
   //try {
     const result = await internals[`${env}DeploySeleniumStandalones`](event);
@@ -39,7 +46,7 @@ exports.provisionSeleniumStandalones = async (event, context) => {
   //}
   const response = {
     //'statusCode': 200,
-    body: { seleniumHubServiceName, requestedSeleniumStandalonesWereProvisioned: result }
+    body: { seleniumStandaloneServiceNames, requestedSeleniumStandalonesWereProvisioned: result }
   };
 
   return response
